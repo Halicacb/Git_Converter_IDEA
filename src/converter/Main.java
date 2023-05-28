288e7afa45c61414c775748949407230675d8650
Halicacb <amoryblaine93@gmail.com>
2023-05-28 18:29:32 +0300

    Conversion of English measurements of length into metric (in Main class)

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         double inches = 10.0;
         double meters = 5.0;
+
+        double convertedMeters = Converter.inchesToMeters(inches);
+        System.out.println(inches + " дюймів = " + convertedMeters + " метрів");
     }
 }
