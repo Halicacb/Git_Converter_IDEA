979ecd88257ad571dfc51b4ee7404a91f5379e84
Halicacb <amoryblaine93@gmail.com>
2023-05-28 17:49:32 +0300

    A method for converting English length measurements to metric

--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -1,5 +1,8 @@
 package converter;
 
 public class Converter {
-
+    public static double inchesToMeters(double inches) {
+        double meters = inches * 0.0254;
+        return meters;
+    }
 }
