17db42f034a9dfb4adf2d30e05324835a39a727a
Halicacb <amoryblaine93@gmail.com>
2023-05-28 19:22:07 +0300

    add value

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         double inches = 100.0;
         double meters = 500.0;
+        double cableLength = 2.0;
 
         double convertedMeters = Converter.inchesToMeters(inches);
         System.out.println(inches + " дюймів = " + convertedMeters + " метрів");
